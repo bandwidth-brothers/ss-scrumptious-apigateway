09a9d707255506ab29f9f164f0f59361212d6297
johnzhengss <john.zheng@smoothstack.com>
2021-09-02 12:31:02 -0500

    BB-82 api-gateway add token filter

--- src/main/java/com/ss/api_gateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/com/ss/api_gateway/config/LoadBalancedRoutesConfig.java
@@ -9,14 +9,15 @@ import org.springframework.context.annotation.Configuration;
 public class LoadBalancedRoutesConfig {
 
     public static final String DEMO_SERVICE = "lb://demo-service";
-    public static final String DEMOAUTH_SERVICE = "lb://demoauth-service";
+    public static final String AUTH_SERVICE = "lb://auth-service";
     public static final String DEMOCUSTOMER_SERVICE = "lb://democustomer-service";
 
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(r -> r.path("/login")
-                        .uri(DEMOAUTH_SERVICE))
+                .route(r -> r.path("/auth" + "/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri(AUTH_SERVICE))
                 .route(r -> r.path("/customer" + "/**")
                         .filters(f -> f.stripPrefix(1))
                         .uri(DEMOCUSTOMER_SERVICE))
