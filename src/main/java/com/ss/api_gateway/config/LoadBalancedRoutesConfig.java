9213f117cacb5d338479a1cc78f0c06d3bc0a650
NickCelesti <nicholas.celesti@smoothstack.com>
2021-09-15 12:54:21 -0400

    fix: updated routes to match existing (and to-be existing) services

--- src/main/java/com/ss/api_gateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/com/ss/api_gateway/config/LoadBalancedRoutesConfig.java
@@ -8,22 +8,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class LoadBalancedRoutesConfig {
 
-    public static final String DEMO_SERVICE = "lb://demo-service";
-    public static final String AUTH_SERVICE = "lb://auth-service";
-    public static final String DEMOCUSTOMER_SERVICE = "lb://democustomer-service";
+    public static final String AUTH_SERVICE = "lb://scrumptious-auth-service";
+    public static final String CUSTOMER_SERVICE = "lb://scrumptious-customer-service";
+    public static final String RESTAURANT_SERVICE = "lb://scrumptious-restaurant-service";
+    public static final String DRIVER_SERVICE = "lb://scrumptious-driver-service";
+    public static final String ORDER_SERVICE = "lb://scrumptious-order-service";
+    public static final String DELIVERY_SERVICE = "lb://scrumptious-delivery-service";
 
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(r -> r.path("/auth" + "/**")
-                        .filters(f -> f.stripPrefix(1))
-                        .uri(AUTH_SERVICE))
-                .route(r -> r.path("/customer" + "/**")
-                        .filters(f -> f.stripPrefix(1))
-                        .uri(DEMOCUSTOMER_SERVICE))
-                .route(r -> r.path("/demo" + "/**")
-                        .filters(f -> f.stripPrefix(1))
-                        .uri(DEMO_SERVICE))
+                .route(r -> r.path("/auth" + "/**").filters(f -> f.stripPrefix(1)).uri(AUTH_SERVICE))
+                .route(r -> r.path("/customer" + "/**").filters(f -> f.stripPrefix(1)).uri(CUSTOMER_SERVICE))
+                .route(r -> r.path("/restaurant" + "/**").filters(f -> f.stripPrefix(1)).uri(RESTAURANT_SERVICE))
+                .route(r -> r.path("/driver" + "/**").filters(f -> f.stripPrefix(1)).uri(DRIVER_SERVICE))
+                .route(r -> r.path("/order" + "/**").filters(f -> f.stripPrefix(1)).uri(ORDER_SERVICE))
+                .route(r -> r.path("/delivery" + "/**").filters(f -> f.stripPrefix(1)).uri(DELIVERY_SERVICE))
                 .build();
     }
 }
